659a37b16d188835e37661a644a2a826add5fd0a
Jason May <jason.a.may@gmail.com>
2010-07-02 16:56:12 -0400

    do some moving around

--- lib/App/Termast/Server/Telnet.pm
+++ lib/App/Termast/Server/Telnet.pm
@@ -1,6 +1,9 @@
 #!::usr::bin::env perl
 package App::Termast::Server::Telnet;
 use Moose;
+use AnyEvent::Socket;
+use AnyEvent::Handle;
+use AE;
 use namespace::autoclean;
 
 =head1 NAME
@@ -14,6 +17,64 @@ App::Termast::Server::Telnet - telnet interface for the termcast server
 
 
 =cut
+
+has handle => (
+    is  => 'ro',
+    isa => 'AnyEvent::Handle',
+);
+
+has server_guard => (
+    is  => 'rw',
+    lazy_build => 1,
+);
+
+sub _build_server_guard {
+    my $self = shift;
+
+tcp_connect 'localhost', 9092, sub {
+    my ($fh) = @_
+        or die "localhost connect failed: $!";
+
+    $h = AnyEvent::Handle->new(
+        fh => $fh,
+        on_read => sub {
+            my ($h, $host, $port) = @_;
+            $h->push_read(
+                json => sub {
+                    my ($h, $data) = @_;
+                    $data->{response} ||= 'null';
+                    if ($data->{response} eq 'sessions') {
+                        my %sessions = %{ $data->{sessions} };
+                        if (keys %sessions) {
+                            warn "something";
+                        }
+                    }
+                    elsif ($data->{response} eq 'stream') {
+                        warn $data->{stream};
+                    }
+                }
+            );
+        },
+        on_error => sub {
+            my ($h, $fatal, $error) = @_;
+            warn $error;
+            die if $fatal;
+        },
+    );
+
+    $h->push_write(
+        json => +{
+            request => 'sessions',
+        }
+    );
+
+    $self->handle($h);
+};
+
+}
+
+sub run { AE::cv->recv }
+
 
 __PACKAGE__->meta->make_immutable;
 
