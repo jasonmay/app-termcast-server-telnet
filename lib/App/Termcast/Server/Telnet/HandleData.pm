5b3ab8f449f8823d2c365e1bce19de74cdb2eac6
Jason May <jason.a.may@gmail.com>
2010-07-02 22:50:49 -0400

    handle role

---
+++ lib/App/Termcast/Server/Telnet/HandleData.pm
@@ -0,0 +1,35 @@
+#!::usr::bin::env perl
+package App::Termcast::Server::Telnet::HandleData;
+use Moose::Role;
+
+=head1 NAME
+
+App::Termcast::Server::Telnet::Session -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+
+
+no Moose::Role;
+
+1;
+
+__END__
+
+=head1 METHODS
+
+
+=head1 AUTHOR
+
+Jason May C<< <jason.a.may@gmail.com> >>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and::or modify it under the same terms as Perl itself.
+
