8881f21a4799e4d988cff80c1110b2f7c16df28e
Jason May <jason.a.may@gmail.com>
2011-02-16 15:23:58 -0500

    reflect reflex switch in runner script

--- bin/termcast-server-telnet.pl
+++ bin/termcast-server-telnet.pl
@@ -1,8 +1,16 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+
+use Cwd;
+
+use lib 'lib';
 use App::Termcast::Server::Telnet;
 
-my $app = App::Termcast::Server::Telnet->new;
+die "Arg required (socket path)" if !@ARGV;
 
-$app->run;
+my $socket = Cwd::abs_path($ARGV[0]);
+
+my $app = App::Termcast::Server::Telnet->new(service_socket => $socket);
+
+$app->run_all();
