9e60b9e194f7c2586c59960f3bd4681ea86b562c
Jason May <jason.a.may@gmail.com>
2011-02-18 16:24:49 -0500

    some attrs and lifecycles

--- lib/App/Termcast/Server/Telnet.pm
+++ lib/App/Termcast/Server/Telnet.pm
@@ -3,25 +3,51 @@ use Moose;
 
 use Reflex::Collection;
 
+use IO::Socket::UNIX;
+
 extends 'Bread::Board::Container';
 
 has '+name' => (default => __PACKAGE__);
+
+has service_socket_path => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has service_socket => (
+    is      => 'ro',
+    isa     => 'FileHandle',
+    lazy    => 1,
+    builder => '_build_service_socket',
+);
+
+sub _build_service_socket {
+    my $self = shift;
+
+    my $socket = IO::Socket::UNIX->new(
+        Peer => $self->service_socket_path,
+    );
+}
 
 sub BUILD {
     container $self => as {
 
         # broadcaster sockets
         service session_pool => (
-            class => 'Reflex::Collection',
+            class     => 'Reflex::Collection',
+            lifecycle => 'Singleton',
         );
 
         # users connected to telnet
         service connection_pool => (
-            class => 'Reflex::Collection',
+            class     => 'Reflex::Collection',
+            lifecycle => 'Singleton',
         );
 
         service telnet_acceptor => (
-            class => 'App::Termcast::Server::Telnet::Acceptor',
+            class     => 'App::Termcast::Server::Telnet::Acceptor',
+            lifecycle => 'Singleton',
             dependencies => [
                 'conenction_pool',
                 'session_pool',
@@ -29,16 +55,19 @@ sub BUILD {
             ],
         );
 
-        service service_connector => (
-            class => 'Reflex::Base',
+        service service_stream => (
+            class     => 'Reflex::Base',
+            lifecycle => 'Singleton',
         );
 
         service telnet_dispatcher => (
-            class => 'App::Termcast::Server::Telnet::Dispatcher::Connection',
+            class     => 'App::Termcast::Server::Telnet::Dispatcher::Connection',
+            lifecycle => 'Singleton',
         );
 
         service service_dispatcher => (
-            class => 'App::Termcast::Server::Telnet::Dispatcher::Service',
+            class     => 'App::Termcast::Server::Telnet::Dispatcher::Service',
+            lifecycle => 'Singleton',
         );
     };
 }
