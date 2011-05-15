6b915f4e88d8c5cd644771facb8f475734c1d381
Jason May <jason.a.may@gmail.com>
2011-05-15 14:12:40 -0400

    don't die on a non-session message

--- lib/App/Termcast/Server/Telnet/Stream/Service.pm
+++ lib/App/Termcast/Server/Telnet/Stream/Service.pm
@@ -31,37 +31,49 @@ sub on_data {
 
     my $data = JSON::decode_json($args->{data});
 
-    if (not ref $data->{sessions} or ref($data->{sessions}) ne 'ARRAY') {
-        warn 'invalid data into service stream: invalid "sessions" key';
-        return;
+    if ($data->{sessions}) {
+        if (not ref $data->{sessions} or ref($data->{sessions}) ne 'ARRAY') {
+            warn 'invalid data into service stream: invalid "sessions" key';
+            return;
+        }
+
+        foreach my $session (@{ $data->{sessions} }) {
+            warn $session->{socket};
+            my $handle = IO::Socket::UNIX->new(
+                Peer => $session->{socket},
+            ) or do {
+                warn $!;
+                next;
+            };
+
+            warn "Connecting to $session->{socket}";
+
+            my %params = (
+                username        => $session->{user},
+                last_active     => $session->{last_active},
+                stream_id       => $session->{session_id},
+                handle_path     => $session->{socket},
+                connection_pool => $self->connection_pool,
+                handle          => $handle,
+                $session->{geometry} ? (
+                    cols => $session->{geometry}->[0],
+                    rows => $session->{geometry}->[1],
+                ) : (),
+            );
+
+            # ugh long class names
+            my $session_class = 'App::Termcast::Server::Telnet::Stream::Session';
+            my $session_object = $session_class->new(%params);
+
+            $self->session_pool->remember_stream(
+                $session->{session_id} => $session_object,
+            );
+
+            $self->remember($session_object);
+        }
     }
-
-    foreach my $session (@{ $data->{sessions} }) {
-        my $handle = IO::Socket::UNIX->new(
-            Peer => $session->{socket},
-        ) or do {
-            warn $!;
-            next;
-        };
-
-        my %params = (
-            username    => $session->{user},
-            last_active => $session->{last_active},
-            session_id  => $session->{session_id},
-            handle      => $handle,
-            $session->{geometry} ? (
-                cols => $session->{geometry}->[0],
-                rows => $session->{geometry}->[1],
-            ) : (),
-        );
-
-        # ugh long class names
-        my $session_class = 'App::Termcast::Server::Telnet::Stream::Session';
-        my $session_object = $session_class->new(%params);
-
-        $self->session_pool->remember_stream(
-            $session->{session_id} => $session_object,
-        );
+    elsif ($data->{notice}) {
+        warn "got a 'notice' message";
     }
 
     #use Data::Dumper::Concise; die Dumper($data);
